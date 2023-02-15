262d5d02dd3e29ca90d2eaa7d80b6d4885a30506
Salvador <Sagarcia2713@gmail.com>
2023-02-14 21:16:43 -0600

    Modified the dark mode colors for accent color and secondary accent color so that it looks better on dark mode. Added a max width to the button of 400 so that the button isn't too large on portrait mode.

--- TodoListSwiftUI/Views/AddView.swift
+++ TodoListSwiftUI/Views/AddView.swift
@@ -23,7 +23,7 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(Color(uiColor: .systemGray6))
+                    .background(Color(uiColor: .secondarySystemBackground))
                     .cornerRadius(10)
                 Button {
                     saveButtonPressed()
